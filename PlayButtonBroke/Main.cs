45e50198bd2609711184a5d614e0028298dca1a5
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-02-12 18:08:35 +0100

    Initial commit

---
+++ PlayButtonBroke/Main.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace PlayButtonBroke
+{
+    [BepInPlugin("srxd.raoul1808.playbuttonbroke", "Play Button Broke", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Patches));
+        }
+    }
+
+    class Patches
+    {
+        [HarmonyPatch(typeof(XDLevelSelectMenuBase), "PlayTrack")]
+        [HarmonyPrefix]
+        private static bool NoTrackPlayageForYou() => false;
+
+        [HarmonyPatch(typeof(XDCustomLevelSelectMenu), "EditTrack")]
+        [HarmonyPrefix]
+        private static bool NoTrackEditageForYouEither() => false;
+    }
+}
